6987010bf3b9aed3b2d52895fc888e7de382a286
Jonnevie <56157123+Jonnevie@users.noreply.github.com>
2022-09-12 17:19:49 +1000

    commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,7 @@ import { useShoppingCart } from "../context/ShoppingCartContext";
 
 
 export function Navbar() {
+
   const { openCart, cartQuantity } = useShoppingCart()
   return (
   <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
